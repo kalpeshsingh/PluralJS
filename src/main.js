82a5f706490ec4e51ec3836ea1877afd2d870a19
Kalpesh Singh <kalpesin@amazon.com>
2020-05-15 07:34:39 +0530

    feat(main.js): add more rules to cover different nouns

--- src/main.js
+++ src/main.js
@@ -2,9 +2,30 @@
 
 /** Helpers **/
 import endsWith from './helpers/endsWith';
+import dictionary from './helpers/dictionary';
 
 const pluralJs = (string) => {
-	return endsWith(string, ['s']);
+	if (dictionary[string]) {
+		return dictionary[string];
+	} else if (endsWith(string, 'us')) {
+		return string.replace(/us$/, 'i');
+	} else if (endsWith(string, 'is')) {
+		return string.replace(/is$/, 'es');
+	} else if (endsWith(string, 'on')) {
+		return string.replace(/on$/, 'a');
+	} else if (endsWith(string, ['s', 'ss', 'sh', 'ch', 'x', 'z', 'o'])) {
+		return `${string}es`;
+	} else if (endsWith(string, ['f', 'fe'])) {
+		return string.replace(/fe|f$/, 'ves');
+	} else if (endsWith(string, 'y')) {
+		if (['a', 'e', 'i', 'o', 'u'].includes(string.substr(-2, 1))) {
+			return `${string}s`;
+		} else {
+			return string.replace(/.$/, 'ies');
+		}
+	} else {
+		return `${string}s`;
+	}
 };
 
 export default pluralJs;
